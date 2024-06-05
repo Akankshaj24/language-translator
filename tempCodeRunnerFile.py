f0dfc42ebf36ad80f2d9ba7a19c48f8f43b5782d
Akanksha Vinayak Jadhav <98791062+Akankshaj24@users.noreply.github.com>
2024-06-05 20:23:56 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+#load  French model
+# model = load_model('english_to_french_model')
+
+# #load Tokenizer
+# with open('english_tokenizer.json') as f:
+#     data = json.load(f)
+#     english_tokenizer = tokenizer_from_json(data)
+    
+# with open('french_tokenizer.json') as f:
+#     data = json.load(f)
+#     french_tokenizer = tokenizer_from_json(data)
